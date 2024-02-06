b802f8def0394e074fbf0ba0fee5d595db4d5430
Starnoh254 <stajohn697@gmail.com>
2024-02-07 00:29:04 +0300

    latest

--- greenblue-frontend-main/src/hooks/useURL.js
+++ greenblue-frontend-main/src/hooks/useURL.js
@@ -1,8 +1,12 @@
 const useURL = () => {
   // const baseUrl = "https://api.greenbluefoundation.org";
-  const baseUrl = "http://localhost:4000";
+  // const baseUrl = "http://localhost:4000";
+  const baseUrl = "https://green-blue.onrender.com"
   // const baseWss = "wss://api.greenbluefoundation.org";
-  const baseWss = "wss://localhost:4000";
+  // const baseWss = "wss://localhost:4000";
+
+  const baseWss = "wsss://green-blue.onrender.com"
+
   return {
     baseUrl,
     baseWss,
